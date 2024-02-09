7029febf423c4e7b9b6d19a56d8f8ebbe2eea809
Hunter <hunterr@h-macbook.wifi.uc.edu>
2024-02-08 19:30:24 -0500

    Initial commit

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+    public static void ShipCostCalculator (String[] args)
+        public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+    }
+}
